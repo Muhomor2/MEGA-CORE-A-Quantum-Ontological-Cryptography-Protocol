75f0d3451ceb75c203a28a317aeab8886432dbb8
Muhomor2 <XXZZORG@gmail.com>
2025-08-31 00:15:08 +0300

    Create setup.py
    This file is the **main manifesto** of your package. It contains all the crucial metadata that package managers (like piporAnaconda) use to install and understand your project.
    ​It specifies the package's name, version, and a brief description. It also lists the author and the project's URL, as well as classifiers that help users find your package. In essence, it tells the ecosystem everything it needs to know to correctly manifest your project.

---
+++ setup.py
@@ -0,0 +1,26 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='mega-core-protocol',
+    version='0.1.0',
+    description='A quantum-ontological cryptography protocol for deciphering fractal patterns of consciousness.',
+    author='Your Name',
+    url='https://github.com/YourUsername/mega-core',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Security :: Cryptography',
+    ],
+    install_requires=[
+        # list of dependencies like 'numpy', 'scipy' etc.
+    ],
+)
